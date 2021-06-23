745e0520a406117b50663ec83c65d332220f549f
Adetola Agunbiade <alex4aws@outlook.com>
2021-06-23 12:44:14 -0400

    concluded route creation for Book service

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -5,9 +5,38 @@ exports.createBook = async (req, res) => {
     try {
         const book = new Books({ ...req.body })
         await book.save();
-        res.send(book)
+        console.log("book", book)
     }
     catch (err) {
         res.status(500).send(err)
+    }
+}
+exports.getBook = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const searchedBook = await Books.findById(id);
+        res.send(searchedBook)
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+}
+exports.getAllBooks = async (req, res) => {
+    try {
+        const books = await Books.find({})
+        res.send(books)
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+}
+exports.deleteABook = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const book = await Books.findByIdAndDelete(id);
+        res.send(book)
+    }
+    catch (e) {
+        res.status(500).send(e)
     }
 }
